88a4f52f9f5c98f29d9703b251d6e88d63df950b
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-03-04 21:32:25 -0300

    Streaming EndPoint

--- src/main/java/com/jeronimo/webfluxdemo/config/RequestHandler.java
+++ src/main/java/com/jeronimo/webfluxdemo/config/RequestHandler.java
@@ -3,6 +3,7 @@ package com.jeronimo.webfluxdemo.config;
 import com.jeronimo.webfluxdemo.dto.Response;
 import com.jeronimo.webfluxdemo.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -26,5 +27,13 @@ public class RequestHandler {
         int input = Integer.valueOf(serverRequest.pathVariable("input"));
         Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
         return ServerResponse.ok().body(responseFlux, Response.class);
+    }
+
+    public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest){
+        int input = Integer.valueOf(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(responseFlux, Response.class);
     }
 }
