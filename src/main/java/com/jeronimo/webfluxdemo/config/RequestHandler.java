177b72e58e63b779a17a1cd34c61120b0796d12d
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-03-04 21:27:34 -0300

    Exposing Mono & Flux via Functional EndPoint

---
+++ src/main/java/com/jeronimo/webfluxdemo/config/RequestHandler.java
@@ -0,0 +1,30 @@
+package com.jeronimo.webfluxdemo.config;
+
+import com.jeronimo.webfluxdemo.dto.Response;
+import com.jeronimo.webfluxdemo.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RequestHandler {
+
+    @Autowired
+    private ReactiveMathService mathService;
+
+    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest){
+        int input = Integer.valueOf(serverRequest.pathVariable("input"));
+        Mono<Response> responseMono = this.mathService.findSquare(input);
+        return ServerResponse.ok().body(responseMono, Response.class);
+    }
+
+
+    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest){
+        int input = Integer.valueOf(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse.ok().body(responseFlux, Response.class);
+    }
+}
