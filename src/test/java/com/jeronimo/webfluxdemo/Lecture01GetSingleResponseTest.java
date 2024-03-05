0a595e7e86e76e3bb960e41f0189a21920cb3d65
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2024-03-05 13:30:15 -0300

    WebClient Bean

---
+++ src/test/java/com/jeronimo/webfluxdemo/Lecture01GetSingleResponseTest.java
@@ -0,0 +1,10 @@
+package com.jeronimo.webfluxdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class Lecture01GetSingleResponseTest extends BaseTest{
+
+    @Autowired
+    private WebClient webClient;
+}
